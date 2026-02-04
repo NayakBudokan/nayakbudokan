e18d91af9adc8edb8e5afced94c733fafbd4a3c3
NayakBudokan <nbka.hyd@gmail.com>
2026-02-04 23:51:29 +0530

    Update typing.js

--- typing.js
+++ typing.js
@@ -1,1 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const elements = document.querySelectorAll(".typing");
+
+    elements.forEach(el => {
+
+        const text = el.getAttribute("data-text");
+        const words = text.split(" ");
+        let index = 0;
+
+        el.textContent = "";
+
+        const interval = setInterval(() => {
+            if (index < words.length) {
+                el.textContent += words[index] + " ";
+                index++;
+            } else {
+                clearInterval(interval);
+            }
+        }, 250); // speed (lower = faster)
+    });
+
+});
 
